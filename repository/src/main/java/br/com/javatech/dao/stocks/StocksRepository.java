f857832f59e12e21e327b70c9b2deefef327b382
iranmzk <iranmpc93@gmail.com>
2022-09-16 11:15:15 -0300

    Integration is working

--- repository/src/main/java/br/com/javatech/dao/stocks/StocksRepository.java
+++ repository/src/main/java/br/com/javatech/dao/stocks/StocksRepository.java
@@ -1,10 +1,10 @@
 package br.com.javatech.dao.stocks;
 
-import br.com.javatech.model.stocks.Stocks;
+import br.com.javatech.dao.stocks.model.StockEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StocksRepository extends MongoRepository<Stocks,String> {
-    Stocks findBySymbolContains(String symbol);
+public interface StocksRepository extends MongoRepository<StockEntity,String> {
+    StockEntity findBySymbolContains(String symbol);
 }
--- services/src/main/java/br/com/javatech/service/stock/ServiceStock.java
+++ services/src/main/java/br/com/javatech/service/stock/ServiceStock.java
@@ -2,33 +2,41 @@ package br.com.javatech.service.stock;
 
 import br.com.javatech.dao.stocks.StocksRepository;
 import br.com.javatech.dao.stocks.mapper.IntegrationToEntity;
-import br.com.javatech.model.stocks.Stocks;
-import br.com.javatech.service.stock.mapper.IntegrationToService;
-import br.com.javatech.service.stock.model.Stock;
+import br.com.javatech.dao.stocks.model.StockEntity;
+import br.com.javatech.service.stock.mapper.EntityToService;
+import br.com.javatech.service.stock.model.StockServiceModel;
+import br.com.javatech.yahoo.YahooFinancesIntegration;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
 public class ServiceStock {
 
     private final StocksRepository repository;
+    private final YahooFinancesIntegration integration;
 
-    public Optional<Stocks> findByNameContains(String symbol){
+    public Optional<StockEntity> findByIdContains(String symbol){
         return repository.findById(symbol);
     }
-    public List<Stock> findAllStock(){
+    public List<StockServiceModel> findAllStock(){
         return repository.findAll()
                 .stream()
-                .map(IntegrationToService::EntityToService)
-                .collect(Collectors.toList());
+                .map(EntityToService::entityToService)
+                .toList();
     }
 
-    public Stocks create(Stocks stocks){
-        return repository.save(stocks);
+    public StockServiceModel findStockIntegration(String symbol){
+        return Optional.ofNullable(integration.findStock(symbol))
+                .map(IntegrationToEntity::integrationToEntity)
+                .map(repository::save)
+                .map(EntityToService::entityToService)
+                .orElseThrow();
+    }
+    public StockEntity create(StockEntity stock){
+        return repository.save(stock);
     }
 }
