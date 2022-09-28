d6f99605b3407148440cf63a8daa004d8418eb1f
iranmzk <iranmpc93@gmail.com>
2022-09-28 15:32:52 -0300

    Test MapStruct e MapStruct no modulo web

--- web/src/main/java/br/com/javatech/controller/stock/facade/ControllerFacade.java
+++ web/src/main/java/br/com/javatech/controller/stock/facade/ControllerFacade.java
@@ -1,6 +1,6 @@
 package br.com.javatech.controller.stock.facade;
 
-import br.com.javatech.controller.stock.mapper.ServiceToController;
+import br.com.javatech.controller.stock.mapper.ServiceToControllerMap;
 import br.com.javatech.controller.stock.model.StockControllerModel;
 import br.com.javatech.service.stock.facade.ServiceFacade;
 import lombok.AllArgsConstructor;
@@ -16,7 +16,7 @@ public class ControllerFacade {
     private ServiceFacade facade;
     public StockControllerModel findStock(String symbol){
         return Stream.of(facade.findStock(symbol))
-                .map(ServiceToController::serviceToController)
+                .map(ServiceToControllerMap::mapCont)
                 .findFirst()
                 .orElseThrow();
     }
@@ -24,7 +24,7 @@ public class ControllerFacade {
     public List<StockControllerModel> findAll() {
         return facade.findAll()
                 .stream()
-                .map(ServiceToController::serviceToController)
+                .map(ServiceToControllerMap::mapCont)
                 .toList();
     }
 
